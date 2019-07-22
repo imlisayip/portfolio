6f5df0f9c1ef9e7bce0bca9cef1bc4f4a06b7743
imlisayip <imlisayip@gmail.com>
2019-07-22 08:22:19 -0700

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lisa Yip's Portfolio`,
+    description: `Showcasing my work, interests, and life in one place.`,
+    author: `Lisa Yip`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
