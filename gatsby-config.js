454f8ba61a635f3322794795076f4d26ad8a5503
imlisayip <imlisayip@gmail.com>
2019-07-24 17:21:39 -0700

    adding google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,49 @@
 module.exports = {
-  siteMetadata: {
-    title: `Lisa Yip`,
-    description: `Showcasing my work, interests, and life in one place.`,
-    author: `Lisa Yip`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Lisa Yip`,
+        description: `Showcasing my work, interests, and life in one place.`,
+        author: `Lisa Yip`,
     },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `ws7eiam1j3y8`,
-        accessToken: `tnOsVmtLU880CtQ03r1LNysyq_0OOQ9oQj8BIAJrURw`
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: `ws7eiam1j3y8`,
+                accessToken: `tnOsVmtLU880CtQ03r1LNysyq_0OOQ9oQj8BIAJrURw`
+            }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "YUA-144578345-1",
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: false,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
