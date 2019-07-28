7e1f38bc404618388fbb1c8b8717ee027606c0f2
imlisayip <imlisayip@gmail.com>
2019-07-27 17:44:10 -0700

    home page, importing text from Contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,41 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 // import "./index.css"
 
-const IndexPage = () => (
-    <Layout>
-        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-        <div className="home">
-            <h1>Hello There</h1>
-            <p>Welcome my awesome blog</p>
-            <div>
-                <p>
-                    Welcome! I'm Lisa. UX Engineer. Intrepid. Book lover. World traveler. Free spirit. Triathlete.
-                        <span>Read more</span> Previously delivered digital brand experiences at {' '}
-                    <a href="https://www.patreon.com/">
-                        <span>Patreon</span>
-                    </a>
-                    ,{' '}
-                    <a href="https://www.bounteous.com/">
-                        <span>Bounteous</span>
-                    </a>
-                    , and{' '}
-                    <a href="http://informatica.com">
-                        <span>Informatica</span>
-                    </a>
-                    .
-                    Welcome to my portfolio. Read more.
-                    </p>
-                <Image />
+const IndexPage = ({ data }) => {
+    const { title, body } = data.allContentfulAboutConcise.edges[0].node;
+
+    return (
+        <Layout>
+            <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+            <div className="home">
+                <div key={title}>{documentToHtmlString(body.json.content[0])}</div>
             </div>
-            <Link to="/blogposts/">View all posts</Link>
-        </div>
-    </Layout>
-)
+            {/* <Link to="/blogposts/">View all posts</Link> */}
+        </Layout >
+    )
+}
 
 export default IndexPage
+
+export const query = graphql`
+    query AboutConciseQuery {
+        allContentfulAboutConcise  {
+            edges {
+                node {
+                    id
+                    title
+                    body {
+                        json
+                    }
+                }
+            }
+        }
+    }
+`
