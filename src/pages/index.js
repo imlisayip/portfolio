403680079f4e63475763e099e46d038ad2ced7e7
imlisayip <imlisayip@gmail.com>
2019-09-20 13:53:21 -0700

    Converting nav to react router

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,47 @@
 import React from "react"
-
+import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import AboutConcise from "../components/aboutConcise"
-import Archives from "../components/archives"
-import CaseStudy from "../components/casestudy"
-import Work from "../pages/work"
+import Work from "../pages/Work"
+import About from "../pages/About"
 // import "./index.css"
+
+function Nav() {
+    return (
+        <ul className='nav'>
+            <li>
+                <NavLink exact activeClassName='active' to='/'>Work</NavLink>
+            </li>
+            <li>
+                <NavLink activeClassName='active' to='/about'>About</NavLink>
+            </li>
+            <li>
+                Contact
+          {/* <NavLink activeClassName='active' to='/popular'>Popular</NavLink> */}
+            </li>
+        </ul>
+    )
+}
 
 class App extends React.Component {
     render() {
         return (
-            <Work />
-            // <Layout>
-            //     <div className="home">
-            //         <AboutConcise />
-            //     </div>
-            //     <div className="caseStudy">
-            //         <CaseStudy />
-            //     </div>
-            //     <div className="archives">
-            //         <Archives />
-            //     </div>
-            // </Layout >
+            <Router>
+                <div className='container'>
+                    <Nav />
+
+                    <Switch>
+                        <Route exact path='/' component={Work} />
+                        <Route exact path='/About' component={About} />
+                        {/* <Route path='/contact' component={Contact} /> */}
+                        <Route render={() => <p>Not Found</p>} />
+                    </Switch>
+                </div>
+            </Router>
         )
     }
 }
-
 
 export default App
 
