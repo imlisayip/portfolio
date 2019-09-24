e28ba7d1c64302cd7b307a4b4c1a4537bc649ca3
imlisayip <imlisayip@gmail.com>
2019-09-24 08:45:37 -0700

    moving nav into header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Router, Link } from '@reach/router'
+import { NavLink } from 'react-router-dom'
 import Contact from './contact'
 
-const Header = ({ siteTitle }) => (
-    <header role="banner">
-        {/* <h1 style={{ margin: 0 }}> */}
-        <Link
-            to="/"
-            style={{
-                // color: `white`,
-                textDecoration: `none`,
-            }}>
-            <h2>{siteTitle}</h2>
-        </Link >
-
-
-        <div className="navigation">
-            <Link to="/">Work</Link>
-            <Link to="about">About</Link>
-            {/* <Link to="travel"><span>Travel</span></Link> */}
-            <Contact />
-            {/* <Link to="now">Now</Link> */}
-        </div>
-
-        {/* TODO: perhaps indicate the page I'm on with bold? */}
-    </header >
+const Header = ({ siteTitle = 'Lisa Yip' }) => (
+    <header>
+        <NavLink to='/' ><h2>{siteTitle}</h2></NavLink>
+        <ul className='navigation'>
+            <li>
+                <NavLink exact activeClassName='active' to='/'>Work</NavLink>
+            </li>
+            <li>
+                <NavLink activeClassName='active' to='/about'>About</NavLink>
+            </li>
+            <li>
+                <Contact />
+            </li>
+        </ul>
+    </header>
 )
 
 Header.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,21 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Switch, NavLink } from "react-router-dom"
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Work from "../pages/Work"
-import About from "../pages/About"
-// import "./index.css"
-
-function Nav() {
-    return (
-        <ul className='nav'>
-            <li>
-                <NavLink exact activeClassName='active' to='/'>Work</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName='active' to='/about'>About</NavLink>
-            </li>
-            <li>
-                Contact
-          {/* <NavLink activeClassName='active' to='/popular'>Popular</NavLink> */}
-            </li>
-        </ul>
-    )
-}
+// import SEO from '../components/seo'
+import Work from '../pages/Work'
+import About from '../pages/About'
+import Header from '../components/header'
+import "../index.scss"
 
 class App extends React.Component {
     render() {
         return (
             <Router>
                 <div className='container'>
-                    <Nav />
-
+                    <Header siteTitle='Lisa Yip' />
                     <Switch>
                         <Route exact path='/' component={Work} />
                         <Route exact path='/About' component={About} />
-                        {/* <Route path='/contact' component={Contact} /> */}
                         <Route render={() => <p>Not Found</p>} />
                     </Switch>
                 </div>
