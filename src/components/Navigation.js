48850437021e60314f5617bd44b1dc63d34762b8
imlisayip <imlisayip@gmail.com>
2019-09-27 12:56:49 -0700

    navigation to use headroom

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,33 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 import Contact from './contact'
+import Headroom from 'react-headroom'
 
 
-const Navigation = ({ siteTitle = 'Lisa Yip' }) => (
-    <header>
-        <NavLink activeClassName='' to='/' >
-            {/* <span> */}
-            <h1>
-                {siteTitle}
-            </h1>
-            {/* </span> */}
-        </NavLink>
-        <ul className='navigation'>
-            <li>
-                <NavLink exact activeClassName='active' to='/'>Work</NavLink>
-            </li>
-            <li>
-                <NavLink activeClassName='active' to='/about'>About</NavLink>
-            </li>
-            <li>
-                <a href='http://lisayip.com/Yip_Lisa_Resume.pdf' target='_blank'>Resume</a>
-            </li>
-            {/* <li>
-                <Contact />
-            </li> */}
-        </ul>
-    </header>
-)
+const Navigation = ({ siteTitle = 'Lisa Yip' }) => {
+
+    return (
+        <Headroom >
+            <header>
+                <NavLink activeClassName='' to='/' ><h1>{siteTitle}</h1> </NavLink>
+                <ul className='navigation'>
+                    <li>
+                        <NavLink exact activeClassName='active' to='/'>Work</NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName='active' to='/about'>About</NavLink>
+                    </li>
+                    <li>
+                        <a href='http://lisayip.com/Yip_Lisa_Resume.pdf' target='_blank' rel="noopener noreferrer">Resume</a>
+                    </li>
+                    {/* <li>
+                    <Contact />
+                </li> */}
+                </ul>
+            </header>
+        </Headroom>
+    )
+}
 
 Navigation.propTypes = {
     siteTitle: PropTypes.string,
