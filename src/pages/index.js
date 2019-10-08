ddbd5216326818e0a1725dd1b31f29e6de8f83bf
imlisayip <imlisayip@gmail.com>
2019-10-08 11:22:24 -0700

    refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,23 @@ import React from 'react'
 // import SEO from '../components/seo'
 
 import Layout from '../components/Layout'
-import Work from './Work'
+// import Work from './Work'
+import AboutConcise from '../components/AboutConcise'
+import CaseStudy from '../components/CaseStudy'
+import Archives from '../components/Archives'
 
 export default () => (
     <Layout>
-        <Work />
+        <div className='home'>
+            <AboutConcise />
+        </div>
+        <div className='showcase'>
+            <div className='block'>
+                <CaseStudy />
+            </div>
+            <div className='block'>
+                <Archives />
+            </div>
+        </div>
     </Layout>
 )
